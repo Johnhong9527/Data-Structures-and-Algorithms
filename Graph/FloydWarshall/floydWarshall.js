f9b45090480ff6482960a2512c6f5be1542a14df
洪海涛 <honghaitzz11@gmail.com>
2018-02-15 11:06:14 +0800

    .

--- Graph/FloydWarshall/floydWarshall.js
+++ Graph/FloydWarshall/floydWarshall.js
@@ -1,70 +1,26 @@
 function ShortestPath(graph) {
 
   this.graph = graph;
-
-  let INF = Number.MAX_SAFE_INTEGER;
-
-  let minDistance = function (dist, visited) {
-
-    let min = INF,
-      minIndex = -1;
-
-    for (let v = 0; v < dist.length; v++) {
-      if (visited[v] == false && dist[v] <= min) {
-        min = dist[v];
-        minIndex = v;
-      }
-    }
-
-    return minIndex;
-  };
-
-  this.dijkstra = function (src) {
-
-    let dist = [],
-      visited = [],
-      length = this.graph.length;
-
-    for (let i = 0; i < length; i++) {
-      dist[i] = INF;
-      visited[i] = false;
-    }
-
-    dist[src] = 0;
-
-    for (let i = 0; i < length - 1; i++) {
-
-      let u = minDistance(dist, visited);
-
-      visited[u] = true;
-
-      for (let v = 0; v < length; v++) {
-        if (!visited[v] && this.graph[u][v] != 0 && dist[u] != INF && dist[u] + this.graph[u][v] < dist[v]) {
-          dist[v] = dist[u] + this.graph[u][v];
-        }
-      }
-    }
-
-    return dist;
-  };
-
   this.floydWarshall = function () {
 
     let dist = [],
       length = this.graph.length,
       i, j, k;
 
+    // 首先，把dist数组初始化为每个顶点之间的权值，因为i到j可能的最短距离就是这些顶点间的权值
     for (i = 0; i < length; i++) {
       dist[i] = [];
       for (j = 0; j < length; j++) {
         dist[i][j] = this.graph[i][j];
       }
     }
-
+    // 通过k，得到i途径顶点0至k，到达j的最短路径
     for (k = 0; k < length; k++) {
       for (i = 0; i < length; i++) {
         for (j = 0; j < length; j++) {
+          // 判断i经过顶点k到达j的路径是否比已有的最短路径更短
           if (dist[i][k] + dist[k][j] < dist[i][j]) {
+            // 如果是更短的路径，则更新最短路径的值
             dist[i][j] = dist[i][k] + dist[k][j];
           }
         }
@@ -73,4 +29,31 @@ function ShortestPath(graph) {
 
     return dist;
   }
+}
+
+let INF = Number.MAX_SAFE_INTEGER;
+let graph = [
+  [0, 2, 4, INF, INF, INF],
+  [INF, 0, 2, 4, 2, INF],
+  [INF, INF, 0, INF, 3, INF],
+  [INF, INF, INF, 0, INF, 2],
+  [INF, INF, INF, 3, 0, 2],
+  [INF, INF, INF, INF, INF, 0]
+];
+console.log(graph)
+console.log(`-----graph----`)
+let shortestPath = new ShortestPath(graph);
+
+let dist = shortestPath.floydWarshall();
+console.log(dist);
+let s = '';
+for (let i = 0; i < dist.length; ++i) {
+  s = '';
+  for (let j = 0; j < dist.length; ++j) {
+    if (dist[i][j] === INF)
+      s += "INF ";
+    else
+      s += dist[i][j] + "   ";
+  }
+  console.log(s);
 }
