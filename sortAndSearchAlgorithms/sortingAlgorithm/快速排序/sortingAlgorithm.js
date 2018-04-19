bf23685239e110fbb95c3dc55ffc712bb04a3be5
洪海涛 <honghaitzz11@gmail.com>
2018-04-19 22:10:17 +0800

    .

--- sortAndSearchAlgorithms/sortingAlgorithm/快速排序/sortingAlgorithm.js
+++ sortAndSearchAlgorithms/sortingAlgorithm/快速排序/sortingAlgorithm.js
@@ -28,13 +28,14 @@ function ArrayList() {
       index = partition(array, left, right);
       // console.log('----')
       if (left < index - 1) {
-        console.log(`left ${left} < index ${index} - 1: ${left < index - 1}`)
+        // console.log(`left ${left} < index ${index} - 1: ${left < index - 1}`)
         quick(array, left, index - 1);
       }
       if (index < right) {
-        console.log(`index ${index} < right ${right}: ${index < right}`)
+        // console.log(`index ${index} < right ${right}: ${index < right}`)
         quick(array, index, right);
       }
+
     }
   };
   /*[3, 5, 1, 6, 4, 7, 2]*/
@@ -56,65 +57,15 @@ function ArrayList() {
       // console.log(`j  ${j}`);
       if (i <= j) {
         // 交换元素，步骤思路
-        /*
-        第一次划分操作： 
-          left = 0
-          right = 6
-          pivot = 3
-            在发生首次元素事件之后：[3, 5, 1, 2, 4, 7, 6]
-              i++ 跟 j++ 是为了跳过已经做了位置交换元素
-            这时候，第一次划分操作还没有结束。
-            第二次比较开始
-              i = 4
-              j = 5
-              pivot = 6
-              array[4] < 6   4 < 6
-              i++;//i=5
-              array[5] > 6    7 > 6
-              j--; // j = 4
-            到了这里， i<=j 的条件不成立了，while语句停止执行 
-              return 5
-        第二次划分操作：
-          left = 0
-          right = 4
-          pivot = 2
-            在发生首次元素事件之后：[1, 5, 3, 2, 4]
-              i++ 跟 j++ 是为了跳过已经做了位置交换元素
-            第二次比较开始
-              i = 0
-              j = 1
-              pivot = 1
-              array[0] < 1    5 < 1 false
-              i++;//i=2
-              array[1] > 1    5 > 1 true
-              j--; // j = 0
-            到了这里， i<=j 的条件不成立了，while语句停止执行 
-              return 1
-        第三次划分操作：
-          left = 1
-          right = 2
-          pivot = 3
-            在发生首次元素事件之后：[3, 2]
-              i++ 跟 j++ 是为了跳过已经做了位置交换元素
-            第二次比较开始
-              i = 1
-              j = 1
-              pivot = 3
-              array[1] < 3    3 < 3 false
-              i++;//i=2
-              array[1] > 3    3 > 3 false
-              j--; // j = 0
-            到了这里， i<=j 的条件不成立了，while语句停止执行 
-              return 2
-        */
         swap(array, i, j);
         i++;
         j--;
         // console.log(`执行交换`);
       }
     }
-    console.log(array);
-    console.log(` `);
+    // console.log(array);
+    // console.log(` `);
+    console.log(`i  ${i}`)
     return i;
   };
   var swap = function (array, index1, index2) {
